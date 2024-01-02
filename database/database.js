f57959e3578390c5f71362410f4fb962588a3b58
SumitKumar121999 <84810173+SumitKumar121999@users.noreply.github.com>
2024-01-02 14:51:22 +0530

    Update database.js

--- database/database.js
+++ database/database.js
@@ -1,28 +1,15 @@
 const mysql = require("mysql")
 
-// var con = mysql.createConnection({
-//   host: process.env.HOST,
-//   user: process.env.USER,
-//   password: process.env.PASSWORD,
-//   database: process.env.DATABASE,
-// });
-
-
 var con = mysql.createConnection({
   host: 'databasenode2.cvkw22kocp5o.ap-south-1.rds.amazonaws.com',
   user: 'admin',
   password: 'root12345',
-  database: 'resultdatabase'
-  //  host: "localhost",
-  // user: "root",
-  // password: "root12345",
-  // database: "resultdatabase"
+  database: 'resultdatabase'  
 });
 
 con.connect(function (err) {
   if (err) throw err;
   console.log("DB Connected !!!");
 });
-
 
 module.exports = con
