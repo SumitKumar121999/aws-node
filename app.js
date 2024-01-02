7aaffc714523f437c9289c34aa0f6737c759941a
SumitKumar121999 <84810173+SumitKumar121999@users.noreply.github.com>
2024-01-02 13:14:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Example app listening at port :${port}`);
 });
 
 
